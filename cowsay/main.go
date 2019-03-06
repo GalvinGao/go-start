aaef154c30185e91a67227b5fb26fa052643ca31
GalvinGao <2403901511@qq.com>
2019-03-06 15:02:04 +0800

    Add Cowsay

---
+++ cowsay/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+import "strings"
+import "os"
+
+func main() {
+	messageTmpl := ` %s
+< %s >
+ %s
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||`
+
+	message := strings.Join(os.Args[1:], " ")
+	if len(message) == 0 {
+		message = "I love Golang!"
+	}
+	messageTop := strings.Repeat("_", len(message))
+	messageBottom := strings.Repeat("-", len(message))
+	_, err := fmt.Printf(messageTmpl, messageTop, message, messageBottom)
+	if err != nil {
+		fmt.Println("wee woo wee woo! Format String failed!")
+	}
+
+}
