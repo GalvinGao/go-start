fe461049ced0a4b166a998c9a2259fda30a8a843
GalvinGao <2403901511@qq.com>
2019-03-07 11:43:52 +0800

    Add a little wiki system

---
+++ web-hello-world/wiki.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"log"
+)
+
+const LISTEN_ON string = ":8085"
+
+type Page struct {
+	Title	string
+	Body	[]byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(PageTitle string) (*Page, error) {
+	filename := PageTitle + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err	
+	}
+	return &Page{Title: PageTitle, Body: body}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles("template_" + tmpl + ".html")
+	t.Execute(w, p)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := strings.Split(r.URL.Path, "/view/")[1]
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := strings.Split(r.URL.Path, "/edit/")[1]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := strings.Split(r.URL.Path, "/save/")[1]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/" + title, http.StatusFound)
+}
+
+func main() {
+	fmt.Println("Starting HTTP server at " + string(LISTEN_ON))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	
+	log.Fatal(http.ListenAndServe(LISTEN_ON, nil))
+}
+
+
+
+
